6fbba91fb51139dfce6c898858f9f323ff09d6fa
Mujapira <malmasmello2@gmail.com>
2024-01-05 02:45:11 -0300

    home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,60 @@
 import { Component } from "@angular/core"
 import { NgIcon, provideIcons } from "@ng-icons/core"
-
+import { CharacterService } from "../../services/api/characters/character.service"
+import { Character } from "../../services/api/models/character"
+import { CommonModule } from "@angular/common"
+import { RouterLink } from "@angular/router"
+import {
+  matKeyboardDoubleArrowRightOutline,
+  matKeyboardDoubleArrowLeftOutline,
+  matKeyboardArrowRightOutline,
+  matKeyboardArrowLeftOutline,
+  matBadgeOutline,
+  matAccountBoxOutline,
+  matMaleOutline,
+  matFemaleOutline,
+  matHelpOutline,
+  matDangerousOutline,
+  matFavoriteOutline,
+  matDoNotDisturbOnOutline,
+  matCategoryOutline,
+  matPinDropOutline,
+  matFavoriteBorderOutline
+} from "@ng-icons/material-icons/outline"
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [NgIcon],
-  providers: [provideIcons({  })],
-
+  imports: [NgIcon, CommonModule, RouterLink],
+  viewProviders: [
+    provideIcons({
+      matKeyboardDoubleArrowRightOutline,
+      matKeyboardDoubleArrowLeftOutline,
+      matKeyboardArrowRightOutline,
+      matAccountBoxOutline,
+      matKeyboardArrowLeftOutline,
+      matBadgeOutline,
+      matMaleOutline,
+      matFemaleOutline,
+      matHelpOutline,
+      matCategoryOutline,
+      matDangerousOutline,
+      matFavoriteOutline,
+      matDoNotDisturbOnOutline,
+      matPinDropOutline,
+      matFavoriteBorderOutline
+    }),
+  ],
   templateUrl: "./home.component.html",
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private service: CharacterService) {}
+
+  characterList:Character[] = [] as Character[]
+
+  ngOnInit() {
+    this.service.getCharacters([1,2,3,4,5]).subscribe((data) => {
+      this.characterList = data
+    })
+  }
+
+}
